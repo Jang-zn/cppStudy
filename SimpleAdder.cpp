f48a21955f967f29614b374d88f053d5b918f535
Jang <zn2309@naver.com>
2021-11-08 10:12:17 +0900

    Initial commit

---
+++ SimpleAdder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int val1;
+    std::cout<<"숫자 입력 : ";
+    std::cin>>val1;
+
+    int val2;
+    std::cout<<"숫자 입력2 : ";
+    std::cin>>val2;
+
+    int result = val1+val2;
+    std::cout<<"result : "<<result<<std::endl;
+    return 1;
+}
+

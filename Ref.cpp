d15b895dd7e329533fcf3f1085066c8ba22a8c53
Jang <zn2309@naver.com>
2021-11-08 11:57:58 +0900

    참조자(Reference)

---
+++ Ref.cpp
@@ -0,0 +1,17 @@
+﻿#include "iostream"
+using namespace std;
+
+int main(){
+    int num1 = 2021;
+    int &num2 = num1;
+    num2 = 3047;
+
+    cout<<"value : "<<num1<<endl;
+    cout<<"ref : "<<num2<<endl;
+
+    cout<<"value : "<<&num1<<endl;
+    cout<<"ref : "<<&num2<<endl;
+
+
+    return 0;
+}

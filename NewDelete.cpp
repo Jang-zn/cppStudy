ff4db60559f18fc36de306821c163cc8f12c01f1
Jang <zn2309@naver.com>
2021-11-08 14:18:10 +0900

    new&delete

---
+++ NewDelete.cpp
@@ -0,0 +1,18 @@
+﻿#include "iostream"
+#include "string.h"
+using namespace std;
+
+char * makeStrAdr(int len){
+    char * str = new char[len];
+    return str;
+}
+
+
+int main(){
+    char * str = makeStrAdr(20);
+    strcpy(str, "GeaZollida");
+    cout<<str<<endl;
+
+    delete[] str;
+    return 0;
+}

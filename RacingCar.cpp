26e6db237d53efebedce0d05ab3d3b321ecba54a
Jang <zn2309@naver.com>
2021-11-08 15:02:34 +0900

    C++에서의 구조체 기초 구조체가 너무 커졌으니 정의부분은 전부 밖으로 빼준다. 구조체 내부의 함수는 inline 함수로 처리되므로 inline 선언도 명시적으로 해준다. 바깥에 함수 정의 시 해당 함수가 어느 구조체에 속하는지 범위연산자 ::로 명시한다.

--- RacingCar.cpp
+++ RacingCar.cpp
@@ -1,51 +1,59 @@
 ﻿#include "iostream"
 using namespace std;
 
+namespace CAR_CONST{
+    enum{
+        ID_LEN = 20,
+        MAX_SPD = 200,
+        FUEL_STEP = 2,
+        ACC_STEP = 10,
+        BRK_STEP = 10
+    };
+}
+using namespace CAR_CONST;
 
 
 struct Car{
-    enum{
-    ID_LEN = 20,
-    MAX_SPD = 200,
-    FUEL_STEP = 2,
-    ACC_STEP = 10,
-    BRK_STEP = 10
-    };
     char gamerID[ID_LEN];
     int fuelGauge;
     int currSpeed;
 
-    void showCarState(){
-        cout<<"ID : "<<gamerID<<endl;
-        cout<<"연료량 : "<<fuelGauge<<"%"<<endl;
-        cout<<"현재속도 : "<<currSpeed<<"km/h"<<endl;
-    }
+    void showCarState();
+    void acc();
+    void brk();
 
-    void acc(){
-        cout<<"Accel"<<endl;
-        if(fuelGauge<=0){
-            return;
-        }else{
-            fuelGauge-=FUEL_STEP;
-        }
-
-        if(currSpeed+ACC_STEP>=MAX_SPD){
-            currSpeed=MAX_SPD;
-            return;
-        }
-        currSpeed+=ACC_STEP;
-    }
-
-    void brk(){
-        cout<<"Break"<<endl;
-        if(currSpeed<BRK_STEP){
-            currSpeed=0;
-            return;
-        }
-        currSpeed-=BRK_STEP;
-    }
 };
 
+
+inline void Car::showCarState(){
+    cout<<"ID : "<<gamerID<<endl;
+    cout<<"연료량 : "<<fuelGauge<<"%"<<endl;
+    cout<<"현재속도 : "<<currSpeed<<"km/h"<<endl;
+}
+
+inline void Car::acc(){
+    cout<<"Accel"<<endl;
+    if(fuelGauge<=0){
+        return;
+    }else{
+        fuelGauge-=FUEL_STEP;
+    }
+
+    if(currSpeed+ACC_STEP>=MAX_SPD){
+        currSpeed=MAX_SPD;
+        return;
+    }
+    currSpeed+=ACC_STEP;
+}
+
+inline void Car::brk(){
+    cout<<"Break"<<endl;
+    if(currSpeed<BRK_STEP){
+        currSpeed=0;
+        return;
+    }
+    currSpeed-=BRK_STEP;
+}
 
 
 int main(){

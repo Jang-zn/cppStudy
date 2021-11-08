7a0f18e16774c232ccf1e5a27899376d0a13561d
Jang <zn2309@naver.com>
2021-11-08 14:53:20 +0900

    C++에서의 구조체 기초 선언한 Car 구조체 내부로 전역함수로 선언된 함수를 집어넣어줌.

--- RacingCar.cpp
+++ RacingCar.cpp
@@ -11,49 +11,51 @@ struct Car{
     char gamerID[ID_LEN];
     int fuelGauge;
     int currSpeed;
+
+    void showCarState(){
+        cout<<"ID : "<<gamerID<<endl;
+        cout<<"연료량 : "<<fuelGauge<<"%"<<endl;
+        cout<<"현재속도 : "<<currSpeed<<"km/h"<<endl;
+    }
+
+    void acc(){
+        cout<<"Accel"<<endl;
+        if(fuelGauge<=0){
+            return;
+        }else{
+            fuelGauge-=FUEL_STEP;
+        }
+
+        if(currSpeed+ACC_STEP>=MAX_SPD){
+            currSpeed=MAX_SPD;
+            return;
+        }
+        currSpeed+=ACC_STEP;
+    }
+
+    void brk(){
+        cout<<"Break"<<endl;
+        if(currSpeed<BRK_STEP){
+            currSpeed=0;
+            return;
+        }
+        currSpeed-=BRK_STEP;
+    }
 };
 
-void showCarState(const Car &car){
-    cout<<"ID : "<<car.gamerID<<endl;
-    cout<<"연료량 : "<<car.fuelGauge<<"%"<<endl;
-    cout<<"현재속도 : "<<car.currSpeed<<"km/h"<<endl;
-}
 
-void acc(Car &car){
-    cout<<"Accel"<<endl;
-    if(car.fuelGauge<=0){
-        return;
-    }else{
-        car.fuelGauge-=FUEL_STEP;
-    }
-
-    if(car.currSpeed+ACC_STEP>=MAX_SPD){
-        car.currSpeed=MAX_SPD;
-        return;
-    }
-        car.currSpeed+=ACC_STEP;
-}
-
-void brk(Car &car){
-    cout<<"Break"<<endl;
-    if(car.currSpeed<BRK_STEP){
-        car.currSpeed=0;
-        return;
-    }
-    car.currSpeed-=BRK_STEP;
-}
 
 int main(){
     Car run99 = {"run99", 100, 0};
-    acc(run99);
-    acc(run99);
-    showCarState(run99);
-    brk(run99);
-    showCarState(run99);
-
+    while(run99.currSpeed<=50){
+        run99.acc();
+    }
+    run99.showCarState();
     Car spd77 = {"spd77", 100, 0};
-    acc(spd77);
-    brk(spd77);
-    showCarState(spd77);
+    spd77.acc();
+    spd77.acc();
+    spd77.acc();
+    spd77.brk();
+    spd77.showCarState();
     return 0;
 }

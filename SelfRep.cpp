6be6ae09c8a78e35657e648c4491392376013192
Jang <zn2309@naver.com>
2021-11-08 17:51:09 +0900

    Self Reference

---
+++ SelfRep.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class SelfRef{
+private:
+    int num;
+public:
+    SelfRef(int n) : num(n){
+        cout<<"객체생성"<<endl;
+    }
+    SelfRef& adder(int n){
+        num+=n;
+        return *this;
+    }
+    SelfRef& showTwoNumber(){
+        cout<<num<<endl;
+        return *this;
+    }
+};
+
+int main(){
+    SelfRef obj(3);
+    SelfRef &ref = obj.adder(2);
+
+    obj.showTwoNumber();
+    ref.showTwoNumber();
+
+    ref.adder(1).showTwoNumber().adder(2).showTwoNumber();
+    obj.showTwoNumber();
+    return 0;
+}

26e3e7808142fefc3487e5b0460c148f2b9959bd
Jang <zn2309@naver.com>
2021-11-08 11:30:22 +0900

    Namespace Alias 설정

---
+++ NameAlias.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+namespace A {
+    namespace B {
+        namespace C{
+            int num1;
+            int num2;
+        }
+    }
+}
+
+
+
+int main(){
+    A::B::C::num1 = 20;
+    A::B::C::num2 = 10;
+
+    namespace abc = A::B::C;
+
+    cout<<abc::num1<<endl;
+    cout<<abc::num2<<endl;
+
+    return 0;
+}

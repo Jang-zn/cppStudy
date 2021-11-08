e59a5f80d491a7be287e7e51a996c6f16493506c
Jang <zn2309@naver.com>
2021-11-08 11:26:47 +0900

    함수 DefaultValue 선언, Inline함수 선언

---
+++ DefaultValue.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int boxVolume(int x, int y=1, int z=1);
+
+int main(){
+    std::cout<<"[3,3,3] : "<<boxVolume(3,3,3)<<std::endl;
+    std::cout<<"[5,5,D] : "<<boxVolume(5,5)<<std::endl;
+    std::cout<<"[7,D,D] : "<<boxVolume(7)<<std::endl;
+//    std::cout<<"[D,D,D] : "<<boxVolume(3,3,3)<<std::endl;
+}
+
+int boxVolume(int x, int y, int z){
+    return x*y*z;
+}
---
+++ InlineFunc.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+inline int sqr(int x){
+    return x*x;
+}
+
+int main(){
+    std::cout<<sqr(5)<<std::endl;
+    std::cout<<sqr(12)<<std::endl;
+    return 0;
+}

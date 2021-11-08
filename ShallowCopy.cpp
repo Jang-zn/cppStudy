f7247aad008c87c2db4901b26542908b12ea75aa
Jang <zn2309@naver.com>
2021-11-09 08:52:38 +0900

    얕은복사 / 깊은복사 얕은복사 : 객체 내부 멤버변수의 주소값들이 복사되는 방식 깊은복사 : 객체 내부 멤버변수들 자체가 복사되어 별개의 객체로 복사되는것
    현재 코드는 얕은복사 상태로 소멸자 호출이 이뤄져서 Debug 에러 발생한다.

---
+++ ShallowCopy.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person{
+private :
+    char * name;
+    int age;
+public :
+    Person(char * name, int age){
+        int len = strlen(name);
+        this->name = new char[len];
+        strcpy(this->name, name);
+        this->age=age;
+    }
+    void showPersonInfo(){
+        cout<<"이름 : "<<this->name<<endl;
+        cout<<"나이 : "<<this->age<<endl;
+    }
+    ~Person(){
+    delete[] name;
+    cout<<"called Destructor"<<endl;
+}
+};
+
+int main (){
+  Person man1("사람1", 30);
+  Person man2(man1);
+  man1.showPersonInfo();
+  man2.showPersonInfo();
+  return 0;
+}

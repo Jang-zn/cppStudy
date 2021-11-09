a3861b43ad36fbee1bd267c84d631ebab6cf5c3e
Jang <zn2309@naver.com>
2021-11-09 15:22:21 +0900

    참조자를 활용하여 객체 저장 기반 배열클래스 생성 -> 초기화 과정에서 임시객체의 얕은복사로 초기화

---
+++ ImproveInit.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class aaa{
+private :
+    int num;
+public :
+    aaa(int n=0) : num(n){
+    cout<<"aaa(int n=0) constructor"<<endl;
+}
+    aaa(const aaa& ref) : num(ref.num){
+        cout<<"aaa(const aaa& ref) constructor"<<endl;
+}
+    aaa& operator=(const aaa& ref){
+        num = ref.num;
+        cout<<"operator=(const aaa& ref)"<<endl;
+        return *this;
+}
+};
+
+class bbb{
+private :
+    aaa mem;
+public :
+    bbb(const aaa& ref) : mem(ref){}
+};
+
+class ccc {
+private :
+    aaa mem;
+public :
+    ccc(const aaa& ref){
+    mem = ref;
+}
+};
+
+int main(){
+    aaa obj1(12);
+    cout<<"------------------------"<<endl;
+    bbb obj2(obj1);
+    cout<<"------------------------"<<endl;
+    ccc obj3(obj1);
+    cout<<"------------------------"<<endl;
+    return 0;
+}

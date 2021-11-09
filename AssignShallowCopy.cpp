cbab1875726499e422c515defe62c6f022f6373b
Jang <zn2309@naver.com>
2021-11-09 14:43:14 +0900

    대입연산의 오버라이딩 -> 얕은복사 문제 해결을 위해서    1. 깊은복사를 하도록 정의함    2. 깊은복사 전에 기존에 할당된 자원의 해제가 이뤄지도록 만들어줌

--- AssignShallowCopy.cpp
+++ AssignShallowCopy.cpp
@@ -7,15 +7,24 @@ private :
     char * name;
     int age;
 public :
-    Person(char* name, int age){
-    this->name = new char[strlen(name)+1];
-    strcpy(this->name, name);
-    this->age = age;
-}
-    void showPersonInfo(){
-    cout<<"이름 : "<<this->name<<endl;
-    cout<<"나이 : "<<this->age<<endl;
-}
+    Person(char* name, int age)
+    {
+        this->name = new char[strlen(name)+1];
+        strcpy(this->name, name);
+        this->age = age;
+    }
+    void showPersonInfo()
+    {
+        cout<<"이름 : "<<this->name<<endl;
+        cout<<"나이 : "<<this->age<<endl;
+    }
+    Person& operator=(const Person& copy) {
+        delete[] name;//자원해제, 메모리누수 방지
+        this->name = new char[strlen(copy.name)+1];
+        strcpy(this->name, copy.name);
+        this->age = copy.age;
+        return *this;
+    }
     ~Person(){
     delete[] name;
     cout<<"called destructor - "<<this<<endl;
@@ -25,6 +34,7 @@ public :
 int main (){
     Person man1("Lee", 30);
     Person man2("Kim", 27);
+    man2.showPersonInfo();
     man2=man1;
     man1.showPersonInfo();
     man2.showPersonInfo();

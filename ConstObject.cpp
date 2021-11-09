110d8113364925dd2c453e7be90ee3c776cf36a9
Jang <zn2309@naver.com>
2021-11-09 10:18:04 +0900

    const 객체에서는 const 선언된 멤버함수만 호출 가능
    -> 객체의 Data를 변경시키지 않겠다는 보증같은거라서 const 선언이 안돼있으면 아예 호출을 못하게 막아놈

---
+++ ConstObject.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Simple {
+private:
+    int num;
+public:
+    Simple(int n) : num(n) {
+
+    }
+
+    Simple &AddNum(int n) {
+        num += n;
+        return *this;
+    }
+
+    void ShowData() const {
+        cout << "num : " << num << endl;
+    }
+};
+int main (){
+    const Simple obj(8); //const 객체 생성
+    //obj.AddNum(5); //const 선언되지 않은 멤버함수라 호출 불가
+    obj.ShowData();
+    return 0;
+}

ef0dbbf1a1f9e40f70acf0af1f7fea65780920a9
Jang <zn2309@naver.com>
2021-11-09 10:59:20 +0900

    클래스 상속 구현 예제

---
+++ UnivStudentInheri.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Person{
+private:
+    int age;
+    char name[50];
+public:
+    Person(int age, char* name):age(age){
+        strcpy(this->name, name);
+    }
+    void whatYourName() const{
+        cout<<"My name : "<<this->name<<endl;
+    }
+    void howOld() const{
+        cout<<"i'm "<<this->age<<"years old"<<endl;
+    }
+};
+
+class UnivStudent : public Person{
+private:
+    char major[50];
+public:
+    UnivStudent(char * name, int age, char* major)
+    : Person(age, name)
+    {
+        strcpy(this->major, major);
+    }
+    void whoAreYou(){
+        whatYourName();
+        howOld();
+        cout<<"major : "<<this->major<<endl;
+    }
+};
+
+int main(){
+    UnivStudent std1("KIM", 24, "Engineering");
+    std1.whoAreYou();
+    UnivStudent std2("LEE", 23, "Chemical");
+    std2.whoAreYou();
+    return 0;
+}

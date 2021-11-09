d5afe5a8232956b39bbf20a045c931a242b86b87
Jang <zn2309@naver.com>
2021-11-09 09:18:08 +0900

    복사생성자의 호출시점 - 메모리공간 할당 후 초기화가 되는 상황에서 호출된다. 1. 객체를 매개변수로 같은 객체를 생성할 때 2. 함수의 매개변수로 객체가 전달될 때 3. 객체를 반환하되 참조형으로 반환하지 않는 경우(반환된 객체를 할당 후 초기화해야됨.->참조형이면 생성된 객체를 참조하도록 함)

---
+++ PassObjCopcon.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class SoSimple{
+private:
+    int num;
+public :
+    SoSimple(int n):num(n) {
+    }
+    SoSimple(const SoSimple& copy) : num(copy.num){
+    cout<<"Called CopyConstructor"<<endl;
+    }
+    void ShowData(){
+        cout<<"num : "<<num<<endl;
+    }
+};
+
+void SimpleFuncObj(SoSimple ob){
+    ob.ShowData();
+}
+int main(){
+    SoSimple obj(7);
+    cout<<"함수 호출 전"<<endl;
+    SimpleFuncObj(obj);//함수 매개변수로 객체가 넘어갈 경우 복사생성자 호출됨.
+    cout<<"함수 호출 후"<<endl;
+    return 0;
+}

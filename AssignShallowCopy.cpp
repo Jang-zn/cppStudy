8e2fc1ebd3e9245b99b06c9812adb14afb2c91c5
Jang <zn2309@naver.com>
2021-11-09 14:22:29 +0900

    대입연산의 오버라이딩 -> default 대입연산자 사용 시 얕은복사가 이뤄져서 소멸자 호출에 문제가 생긴다.    -> 복사 생성자에서의 문제와 같음.

---
+++ AssignShallowCopy.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person{
+private :
+    char * name;
+    int age;
+public :
+    Person(char* name, int age){
+    this->name = new char[strlen(name)+1];
+    strcpy(this->name, name);
+    this->age = age;
+}
+    void showPersonInfo(){
+    cout<<"이름 : "<<this->name<<endl;
+    cout<<"나이 : "<<this->age<<endl;
+}
+    ~Person(){
+    delete[] name;
+    cout<<"called destructor - "<<this<<endl;
+}
+};
+
+int main (){
+    Person man1("Lee", 30);
+    Person man2("Kim", 27);
+    man2=man1;
+    man1.showPersonInfo();
+    man2.showPersonInfo();
+    return 0;
+}

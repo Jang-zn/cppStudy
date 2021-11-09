8b036b77ec2b4cd0f206835afaa4904a57405256
Jang <zn2309@naver.com>
2021-11-09 16:47:44 +0900

    간단한 템플릿 사용

---
+++ Template/AddFuction.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+T add(T num1, T num2){
+    return num1+num2;
+}
+
+int main(){
+    cout<<add<int>(1,5)<<endl;
+    cout<<add<double>(2.9,6.8)<<endl;
+    cout<<add<int>(3.2,3.2)<<endl;
+    return 0;
+}

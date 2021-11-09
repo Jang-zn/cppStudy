415261e29ce30c603d1d0c2c39c2bcc4c0e68854
Jang <zn2309@naver.com>
2021-11-09 09:56:10 +0900

    반환되는 임시객체의 생성과 삭제시점2

---
+++ TempObjDead.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+class Temp{
+private :
+    int num;
+public :
+    Temp(int n) : num(n){
+        cout<<"Create New Object "<<this<<endl;
+    }
+    Temp(const Temp& copy) : num(copy.num){
+        cout<<"Create Copy Object "<<this<<endl;
+    }
+    ~Temp(){
+        cout<<"Delete Obj "<<this<<endl;
+    }
+};
+
+Temp TempFuncObj(Temp t){
+    cout<<"Parm ADR : "<<&t<<endl;
+    return t;
+}
+
+int main(){
+    Temp t(7);
+    TempFuncObj(t);
+    cout<<endl;
+    Temp tempRef = TempFuncObj(t);
+    cout<<"Return Obj : "<<&tempRef<<endl;
+    return 0;
+
+}

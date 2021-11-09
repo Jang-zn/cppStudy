8df23215478e47baa1c8a66489408517b4511d02
Jang <zn2309@naver.com>
2021-11-09 09:06:18 +0900

    얕은복사 / 깊은복사 얕은복사 : 객체 내부 멤버변수의 주소값들이 복사되는 방식 깊은복사 : 객체 내부 멤버변수들 자체가 복사되어 별개의 객체로 복사되는것
    복사생성자의 재정의로 깊은복사가 이뤄지도록 만들어줌
    *String 복사시 길이+1로 복사해야됨..

--- ShallowCopy.cpp
+++ ShallowCopy.cpp
@@ -9,10 +9,16 @@ private :
 public :
     Person(char * name, int age){
         int len = strlen(name);
-        this->name = new char[len];
+        this->name = new char[len+1];
         strcpy(this->name, name);
         this->age=age;
     }
+
+    Person(const Person& p) : age(p.age){
+        this->name = new char[strlen(p.name)+1];
+        strcpy(this->name,p.name);
+    }
+
     void showPersonInfo(){
         cout<<"이름 : "<<this->name<<endl;
         cout<<"나이 : "<<this->age<<endl;
@@ -25,7 +31,7 @@ public :
 
 int main (){
   Person man1("사람1", 30);
-  Person man2(man1);
+  Person man2=man1;
   man1.showPersonInfo();
   man2.showPersonInfo();
   return 0;
